2d0889397ddf116dccfcd6f698508e596e7aa24e
Rahul Paul <rahulpaul9914@gmail.com>
2025-07-28 19:59:41 +0600

    Update localization api

--- components/InstructorComponent.tsx
+++ components/InstructorComponent.tsx
@@ -9,14 +9,14 @@ export default function InstructorComponent ({ instructorData }: InstructorCompo
             //         COURSE INSTRUCTOR PART START  
             //    =====================================--> */}
               <section className="container">
-                  <div className="mb-7 pt-2">
+                  <div className="mb-7 pt-2 flex flex-col gap-3">
                       <div className="pt-4 pb-2">
-                          <h2 className="mb-4 text-xl md:text-2xl font-semibold">{instructorData.name}</h2>
+                          <h2 className="text-xl md:text-2xl font-semibold">{instructorData.name}</h2>
                       </div>
                         {
                          instructorData.values.map((instructor:instructorValue) => ( 
                             <div key={instructorData.order_idx} className="md:p-5 md:border border-[#dbe1eb] md:rounded-md flex items-center gap-4"> 
-                              <div className="w-[73px] h-[73px] rounded-full overflow-hidden">
+                              <div className="w-[73px] h-[73px] rounded-full overflow-hidden aspect-square flex-shrink-0">
                                <img className="w-full h-full" src={instructor.image} alt="" />
                               </div>
                               <div>
